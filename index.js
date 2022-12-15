aa2c6257b2f29ed0ac6e5d0e82d0668050dd4194
Enes <enessalihbcelebi@gmail.com>
2022-12-15 12:19:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+let drivers = ["ali", "veli", "mete"]
+const findMatching = (drivers, string) => drivers.filter((drivers)=> drivers.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (drivers,string) => drivers.filter((drivers)=> drivers.charAt(0) === string.charAt(0));
+
+const matchName = (drivers,string)=>  drivers.filter((drivers)=>drivers.name ===  string)
